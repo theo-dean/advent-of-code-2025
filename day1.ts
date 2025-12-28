d6aedd306e947e7857d1193f124356907b33553c
Theo <theodeancsee@gmail.com>
2025-12-28 20:53:10 +1100

    package locks

--- day1.ts
+++ day1.ts
@@ -24,7 +24,25 @@ const part1 = (input: string): number => {
     return nilCount;
 };
 
+const part2 = (input: string): number => {
+    let nilCount = 0;
+    let position = 50;
+    let prevPosition = 50;
+
+    for (const line of input.split("\n")) {
+        const magnitude = parseInt(line.slice(1));
+        position += line[0] === "L" ? -magnitude : magnitude;
+        const adjustedPosition = ((position % 100) + 100) % 100;
+        if (adjustedPosition === 0 || ((position > 99 || position < 0) && prevPosition !== 0)) nilCount++;
+
+        position = adjustedPosition;
+        prevPosition = adjustedPosition;
+    }
+    return nilCount;
+};
+
 
 const day1data = fs.readFileSync("day1.input").toString();
 
 console.log(part1(day1data));
+console.log(part2(day1data));
