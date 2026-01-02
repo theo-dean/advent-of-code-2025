eb4350d7d2e396d3c56612c6f90e54760711141e
Theo <theodeancsee@gmail.com>
2026-01-02 17:42:23 +1100

    part 2

--- day1.ts
+++ day1.ts
@@ -11,14 +11,19 @@ const sampleInput = "L68\n" +
     "R14\n" +
     "L82"
 
+type Direction = "L" | "R";
+
 const part1 = (input: string): number => {
     let nilCount = 0;
     let position = 50;
 
     for (const line of input.split("\n")) {
-        const magnitude = parseInt(line.slice(1));
-        position += line[0] === "L" ? -magnitude : magnitude;
-        position = ((position % 100) + 100) % 100;
+        const instruction = parseInstruction(line);
+        let count;
+        for (count = 0; count < instruction.magnitude; count++) {
+            position = moveDial(position, instruction.direction, 0, 99);
+            //console.log(position);
+        }
         if (position === 0) nilCount++;
     }
     return nilCount;
@@ -27,19 +32,30 @@ const part1 = (input: string): number => {
 const part2 = (input: string): number => {
     let nilCount = 0;
     let position = 50;
-    let prevPosition = 50;
 
     for (const line of input.split("\n")) {
-        const magnitude = parseInt(line.slice(1));
-        position += line[0] === "L" ? -magnitude : magnitude;
-        const adjustedPosition = ((position % 100) + 100) % 100;
-        if (adjustedPosition === 0 || ((position > 99 || position < 0) && prevPosition !== 0)) nilCount++;
-
-        position = adjustedPosition;
-        prevPosition = adjustedPosition;
+        const instruction = parseInstruction(line);
+        let count;
+        for (count = 0; count < instruction.magnitude; count++) {
+            position = moveDial(position, instruction.direction, 0, 99);
+            if (position === 0) nilCount++;
+        }
     }
     return nilCount;
 };
+
+const parseInstruction = (instruction: string): {direction: Direction, magnitude: number} => {
+    const magnitude = parseInt(instruction.slice(1));
+    const direction = instruction[0] as Direction; // trust me bro
+    return {magnitude, direction};
+}
+
+const moveDial = (startPosition: number, direction: Direction, lowerBound: number, upperBound: number): number => {
+    const newPos = direction === "L" ? startPosition - 1 : startPosition + 1;
+    if (newPos < lowerBound) return upperBound;
+    if (newPos > upperBound) return lowerBound;
+    return newPos;
+}
 
 
 const day1data = fs.readFileSync("day1.input").toString();
